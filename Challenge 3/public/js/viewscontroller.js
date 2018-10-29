e6655a689896e34f41e8574255edad8b927d7150
jazz1228 <juan.zapata19@udea.edu.co>
2018-10-28 19:45:52 -0500

    challenge 3

---
+++ Challenge 3/public/js/viewscontroller.js
@@ -0,0 +1,179 @@
+$(document).ready(function(){
+    $("#rate").text("RATE THIS BOOK");
+    $("#book1, #wallpa i").hover(function(){$("#information").show();
+    $("#wallpa").show();
+    $("#wallpa2,#wallpa3, #wallpa4, #wallpa5, #wallpa6, #wallpa7, #wallpa8").hide();
+    $("#information").css({"top":"0%"});
+    $("#information").css({"left":"19.5%"});
+    $("#booktitle").text("YOU DON'T KNOW JS");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    })
+       
+
+    $("*").click(function(){
+       $("#information").hide();
+       $("#wallpa, #wallpa2, #wallpa3, #wallpa4, #wallpa5, #wallpa6, #wallpa7, #wallpa8").hide();
+       
+    }) 
+   
+   
+
+    $("#book2").hover(function(){$("#information").show();
+    $("#wallpa2").show();
+    $("#wallpa, #wallpa3, #wallpa4, #wallpa5, #wallpa6, #wallpa7, #wallpa8").hide();
+    $("#information").css({"top":"0%"});
+    $("#information").css({"left":"46.5%"});
+    $("#booktitle").text("DON QUIJOTE DE LA MANCHA");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+
+   
+
+    $("#book3").hover(function(){$("#information").show();
+    $("#wallpa3").show();
+    $("#wallpa,#wallpa2, #wallpa4, #wallpa5, #wallpa6, #wallpa7, #wallpa8").hide();
+    $("#information").css({"left":"12.5%"});
+    $("#information").css({"top":"0%"});
+    $("#booktitle").text("ROMEO AND JULIET");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+   
+
+    $("#book4").hover(function(){$("#information").show();
+    $("#wallpa4").show();
+    $("#wallpa, #wallpa3,#wallpa2,  #wallpa5, #wallpa6, #wallpa7, #wallpa8").hide();
+    $("#information").css({"top":"0%"});
+    $("#information").css({"left":"40%"});
+    $("#booktitle").text("BIG LITTLE LIES");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+   
+
+    $("#book5").hover(function(){$("#information").show();
+    $("#wallpa5").show();
+    $("#wallpa, #wallpa3,#wallpa2, #wallpa4,  #wallpa6, #wallpa7, #wallpa8").hide();
+    $("#information").css({"top":"43%"});
+    $("#information").css({"left":"19.5%"});
+    $("#booktitle").text("REVIVAL");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+  
+
+    $("#book8").hover(function(){$("#information").show();
+    $("#wallpa8").show();
+    $("#wallpa, #wallpa3,#wallpa2, #wallpa4, #wallpa5, #wallpa6, #wallpa7").hide();
+    $("#information").css({"top":"43%"});
+    $("#information").css({"left":"46.5%"});
+    $("#booktitle").text("THE RED KNIGHT");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+  
+  
+
+    $("#book6").hover(function(){$("#information").show();
+    $("#wallpa6").show();
+    $("#wallpa, #wallpa3,#wallpa2, #wallpa4, #wallpa5, #wallpa7, #wallpa8").hide();
+    $("#information").css({"top":"43%"});
+    $("#information").css({"left":"12.5%"});
+    $("#booktitle").text("THUS SPOKE ZARATHUSTRA");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+   
+
+    $("#book7").hover(function(){$("#information").show();
+    $("#wallpa7").show();
+    $("#wallpa, #wallpa3,#wallpa2, #wallpa4, #wallpa5, #wallpa6, #wallpa8").hide();
+    $("#information").css({"top":"43%"});
+    $("#information").css({"left":"40%"});
+    $("#booktitle").text("EUPHORIA");
+    $("#bookdate").text("YOU DON'T KNOW JS");
+    $("#booknovel").text("YOU DON'T KNOW JS");
+    $("#bookauthor").text("YOU DON'T KNOW JS");
+    $("#bookpages").text("YOU DON'T KNOW JS");
+    $("#booksummary").text("YOU DON'T KNOW JS");
+    $("#bookdescrip").text("YOU DON'T KNOW JS");
+    $("#bookrating").text("YOU DON'T KNOW JS");
+    $("#bookrecommended").text("YOU DON'T KNOW JS");
+    
+    
+
+    })
+    
+    
+
+    
+
+       });
